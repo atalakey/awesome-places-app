59fc835361c410e0d7a7d6fc4db482b4e08d1721
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-06-22 21:23:44 +0300

    Configuring the place view page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
 
 import { AddPlacePage } from '../add-place/add-place';
-import { Place } from '../../models/place';
+import { PlacePage } from '../place/place';
 import { PlacesService } from '../../services/places';
+import { Place } from '../../models/place';
 
 @Component({
   selector: 'page-home',
@@ -13,12 +14,17 @@ export class HomePage {
   addPlacePage = AddPlacePage;
   places: Place[] = [];
 
-  constructor(private navCtrl: NavController,
+  constructor(private modalCtrl: ModalController,
               private placesService: PlacesService) {
 
   }
 
   ionViewWillEnter() {
     this.places = this.placesService.loadPlaces();
+  }
+
+  onOpenPlace(place: Place, index: number) {
+    const modal = this.modalCtrl.create(PlacePage, {place: place, index: index});
+    modal.present();
   }
 }
--- src/pages/place/place.ts
+++ src/pages/place/place.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams, ViewController } from 'ionic-angular';
+import { Place } from '../../models/place';
 
-/**
- * Generated class for the PlacePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { PlacesService } from '../../services/places';
 
 @IonicPage()
 @Component({
@@ -14,12 +10,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'place.html',
 })
 export class PlacePage {
+  place: Place;
+  index: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navParams: NavParams,
+              private viewCtrl: ViewController,
+              private placesService: PlacesService) {
+    this.place = this.navParams.get('place');
+    this.index = this.navParams.get('index');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PlacePage');
+  onLeave() {
+    this.viewCtrl.dismiss();
   }
 
+  onDelete() {
+    this.placesService.deletePlace(this.index);
+    this.onLeave();
+  }
 }
