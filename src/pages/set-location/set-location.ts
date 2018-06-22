a42190391bccc98317f7cc9eaf1f518a29d66e70
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-06-22 11:55:48 +0300

    Allowing the user to place a marker on the map

--- src/pages/set-location/set-location.ts
+++ src/pages/set-location/set-location.ts
@@ -10,8 +10,14 @@ import { Location } from '../../models/location';
 })
 export class SetLocationPage {
   location: Location;
+  marker: Location;
 
   constructor(private navParams: NavParams) {
     this.location = this.navParams.get('location');
+  }
+
+  onSetMarker(event: any) {
+    console.log(event);
+    this.marker = new Location(event.cords.lat, event.cords.lng);
   }
 }
